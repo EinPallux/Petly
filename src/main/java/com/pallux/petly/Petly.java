462619b74f7988df4195458100d0d4df2e5c5912
Pallux <keinnule@gmail.com>
2026-05-26 22:05:59 +0200

    Initial commit

---
+++ src/main/java/com/pallux/petly/Petly.java
@@ -0,0 +1,16 @@
+package com.pallux.petly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Petly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
